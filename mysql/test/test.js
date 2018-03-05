b3f54a1c58ca09acb6d2965719daa1be1e0f51da
Steve <steve@roaringdevelopment.com>
2018-03-05 14:46:20 -0700

    remove console.log

--- mysql/test/test.js
+++ mysql/test/test.js
@@ -77,13 +77,9 @@ describe.only('SQL', function() {
 
 		//These variables shoud persist between lambda invocations
 		let event = "Community";
-
 		let botId = "Community_snapshotter";
 		let timestamp = moment();
-		let streamContinuation = 'z/' + timestamp.format("YYYY/MM/DD");
-		let batch = timestamp.format("YYYY/MM/DD_") + timestamp.valueOf();
 		//END
-
 
 		let stream = mysqlLoader.nibble({
 			host: "localhost",
@@ -118,22 +114,20 @@ describe.only('SQL', function() {
 			});
 		}), ls.toS3GzipChunks(event, {
 			useS3Mode: true,
-			snapShot: true,
 			time: {
 				minutes: 1
 			},
-			prefix: batch
+			prefix: "_snapshot/" + timestamp.format("YYYY/MM_DD_") + timestamp.valueOf()
 		}, function(done, push) {
 			push({
 				_cmd: 'registerSnapshot',
 				event: event,
-				start: batch,
-				end: batch + "0",
-				continueFrom: streamContinuation
+				start: timestamp.valueOf(),
+				next: timestamp.clone().startOf('day').valueOf()
 			});
 			done();
 		}), ls.toLeo(botId, {
-			snapshot: "z/" + batch
+			snapshot: timestamp.valueOf()
 		}), (err) => {
 			console.log("all done");
 			console.log(err);
