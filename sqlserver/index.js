ea66fe0b2f884c657278ed1b2e51e3c58c4247ed
Francis Lewis <flewis@leoinsights.com>
2018-03-05 11:37:43 -0700

    * Added transoform to the loader to enable working with custom objects from the database results.  * Some code written by Clint to fix the issues with going over 5k records and having to do this in multiple batches - we would never stop processing results unless stopped and started again.  * Added an error message in case the query on our bot is incorrect. Giving a bit more insight on what went wrong.  * Changed the WHERE clause on the changes table to be able to handle id's that could be less than what we've processed previously. This works with our way of ordering, by: change_version, id

--- sqlserver/index.js
+++ sqlserver/index.js
@@ -4,9 +4,18 @@ const sqlLoader = require("../lib/sql/loader");
 const PassThrough = require("stream").PassThrough;
 const logger = require("leo-sdk/lib/logger")("sqlserver");
 
+// require("leo-sdk/lib/logger").configure(/.*/, {
+// 	all: true
+// });
 module.exports = {
 	load: function (config, sql, domain, opts) {
 		return sqlLoader(() => connect(config), sql, domain, opts);
+
+		// Possible solution if the above doesn't work correctly. I didn't find the below to work correctly, but don't
+		// want to destroy it we have a change to test another 5k+ records
+		// return sqlLoader(() => connect(config), sql, domain, Object.assign({
+		// getEid: (id, obj, stats)=>stats.end.replace(/\..*/, "." + id)
+		// }, opts));
 	},
 	streamChanges: function (config, tables, opts = {}) {
 		let client = connect(config);
@@ -23,8 +32,8 @@ module.exports = {
 			}
 		};
 
-		if (opts.start == undefined) {
-			console.error(`Start is a required parameter`)
+		if (opts.start === undefined) {
+			console.error(`Start is a required parameter`);
 			process.exit();
 		}
 
@@ -37,11 +46,12 @@ module.exports = {
 			obj.payload[t] = [];
 			let query = `SELECT '${t}' as tableName, ${tables[t]} as id, SYS_CHANGE_VERSION __SYS_CHANGE_VERSION
 					 FROM  CHANGETABLE(CHANGES ${t}, ${version - 1}) AS CT  
-					 where (SYS_CHANGE_VERSION <> ${version} OR ${tables[t]} > ${offset})`;
+					 where SYS_CHANGE_VERSION > ${version} OR (SYS_CHANGE_VERSION = ${version} AND ${tables[t]} > ${offset})`;
 			logger.log(query);
 			return query;
 		});
 		client.query(sqlTables.join(" UNION ") + ' order by SYS_CHANGE_VERSION asc, id asc', (err, result) => {
+			logger.log(sqlTables.join(" UNION ") + ' order by SYS_CHANGE_VERSION asc, id asc');
 			if (!err) {
 				result.forEach(r => {
 					let eid = `${r.__SYS_CHANGE_VERSION}.${r.id}`;
@@ -51,11 +61,11 @@ module.exports = {
 					obj.eid = eid;
 					obj.payload[r.tableName].push(r.id);
 				});
-				//console.log(obj)
 				stream.write(obj);
 			} else {
-				console.log(err);
+				throw new Error(err);
 			}
+
 			stream.end();
 		});
 
