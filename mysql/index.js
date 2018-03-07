e20ee1be14176e90ee0b84f57bfff46abd6a9785
Clint Zirker <czirker@leoinsights.com>
2018-03-07 12:08:33 -0700

    Added checksum api to connectors

--- mysql/index.js
+++ mysql/index.js
@@ -2,12 +2,16 @@
 const connect = require("./lib/connect.js");
 const sqlLoader = require("../lib/sql/loader");
 const sqlNibbler = require("../lib/sql/nibbler");
+const checksum = require("../lib/sql/checksum");
 
 module.exports = {
-	load: function(config, sql, domain) {
+	load: function (config, sql, domain) {
 		return sqlLoader(() => connect(config), sql, domain);
 	},
-	nibble: function(config, table, id) {
+	nibble: function (config, table, id) {
 		return sqlNibbler(connect(config), table, id);
+	},
+	checksum: function (config) {
+		return checksum(connect(config));
 	}
 };
