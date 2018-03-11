d2e8bdd9fda476999f977b222b7c02c7502b7c2a
Steve <steve@roaringdevelopment.com>
2018-03-10 17:07:09 -0700

    added backoff failure handling

--- postgres/lib/binlogreader.js
+++ postgres/lib/binlogreader.js
@@ -4,6 +4,7 @@ const async = require("async");
 
 const connect = require("./connect.js");
 const test_decoding = require("./test_decoding.js");
+var backoff = require("backoff");
 
 let count = 1;
 
@@ -19,86 +20,125 @@ module.exports = {
 		let pass = new PassThrough({
 			objectMode: true
 		});
-		let query = connect(config).query;
-		let client = new pg.Client(Object.assign({}, config, {
-			replication: 'database'
-		}));
-		let dieError = function(err) {
+
+		var retry = backoff.fibonacci({
+			randomisationFactor: 0,
+			initialDelay: 2000,
+			maxDelay: 60000
+		});
+		retry.failAfter(100);
+		retry.on('backoff', function(number, delay) {
+			console.log(`Going to try to connect again in ${delay} ms`);
+		});
+		retry.once('fail', (err) => {
 			console.log(err);
 			pass.emit(err);
 			pass.destroy();
-			client.removeAllListeners();
-			client.end();
-		};
-		client.on('error', dieError);
-		client.connect(function(err) {
-			if (err) return dieError(err);
-			query(`SELECT * FROM pg_replication_slots where slot_name = $1`, [opts.slot_name], (err, result) => {
-				if (err) return dieError(err);
-				let tasks = [];
-				if (!result.length) {
-					lastLsn = '0/00000000';
-					tasks.push(done => query(`SELECT * FROM pg_create_logical_replication_slot($1, 'test_decoding')`, [opts.slot_name], err => {
-						if (err) return dieError(err);
-						query(`SELECT * FROM pg_replication_slots where slot_name = $1`, [opts.slot_name], (err, result) => {
-							if (err) return dieError(err);
-							if (result.length != 1) return dieError(err);
+		});
+		let reportBackTimeout = null;
 
-							lastLsn = result[0].confirmed_flush_lsn;
-							done();
-						});
-					}));
-				} else {
-					lastLsn = result[0].confirmed_flush_lsn;
+		retry.on('ready', function(number, delay) {
+			let wrapperClient = connect(config);
+			let client = new pg.Client(Object.assign({}, config, {
+				replication: 'database'
+			}));
+			let dieError = function(err) {
+				clearTimeout(reportBackTimeout);
+				if (client) {
+					client.removeAllListeners();
+					try {
+						wrapperClient.end(err => {});
+					} catch (e) {
+						console.log("Cannot end WrapperClient");
+					}
+					try {
+						client.end(err => {});
+					} catch (e) {
+						console.log("Cannot end client");
+					}
+					client = null;
+					wrapperClient = null;
+					retry.backoff(err);
 				}
-				async.series(tasks, (err) => {
-					console.log("HOWDY");
+			};
+			client.on('error', dieError);
+			client.connect(function(err) {
+				console.log("Trying to connect ");
+				if (err) return dieError(err);
+				wrapperClient.query(`SELECT * FROM pg_replication_slots where slot_name = $1`, [opts.slot_name], (err, result) => {
 					if (err) return dieError(err);
+					let tasks = [];
+					if (!result.length) {
+						lastLsn = '0/00000000';
+						tasks.push(done => query(`SELECT * FROM pg_create_logical_replication_slot($1, 'test_decoding')`, [opts.slot_name], err => {
+							if (err) return dieError(err);
+							wrapperClient.query(`SELECT * FROM pg_replication_slots where slot_name = $1`, [opts.slot_name], (err, result) => {
+								if (err) return dieError(err);
+								if (result.length != 1) return dieError(err);
 
-					client.query(`START_REPLICATION SLOT leo_replication LOGICAL ${lastLsn}`, (err, result) => {
+								lastLsn = result[0].confirmed_flush_lsn;
+								done();
+							});
+						}));
+					} else {
+						lastLsn = result[0].confirmed_flush_lsn;
+					}
+					async.series(tasks, (err) => {
+						console.log("HOWDY");
 						if (err) return dieError(err);
-					});
-					client.connection.once('replicationStart', function() {
-						let e = function() {
-							standbyStatusUpdate(client, lastLsn);
-							setTimeout(e, opts.keepalive);
-						};
-						e();
-						client.connection.on('copyData', function(msg) {
-							if (msg.chunk[0] == 0x77) { // XLogData
-								count++;
-								if (count == 10000) {
-									standbyStatusUpdate(client, lastLsn);
-									count = 1;
+
+						client.query(`START_REPLICATION SLOT leo_replication LOGICAL ${lastLsn}`, (err, result) => {
+							if (err) return dieError(err);
+						});
+						client.connection.once('replicationStart', function() {
+							retry.reset();
+							let e = function() {
+								if (reportBackTimeout) {
+									clearTimeout(reportBackTimeout);
+								}
+								standbyStatusUpdate(client, lastLsn);
+								reportBackTimeout = setTimeout(e, opts.keepalive);
+							};
+							e();
+							client.connection.on('error', dieError);
+							client.connection.on('copyData', function(msg) {
+
+								if (msg.chunk[0] == 0x77) { // XLogData
+									count++;
+									if (count == 10000) {
+										standbyStatusUpdate(client, lastLsn);
+										count = 1;
+									}
+
+									let lsn = (msg.chunk.readUInt32BE(1).toString(16).toUpperCase()) + '/' + (msg.chunk.readUInt32BE(5).toString(16).toUpperCase());
+									lastLsn = lsn;
+									pass.write({
+										lsn,
+										log: test_decoding.parse(msg.chunk.slice(25).toString('utf8'))
+									});
+								} else if (msg.chunk[0] == 0x6b) { // Primary keepalive message
+									let lsn = (msg.chunk.readUInt32BE(1).toString(16).toUpperCase()) + '/' + (msg.chunk.readUInt32BE(5).toString(16).toUpperCase());
+									var timestamp = Math.floor(msg.chunk.readUInt32BE(9) * 4294967.296 + msg.chunk.readUInt32BE(13) / 1000 + 946080000000);
+									var shouldRespond = msg.chunk.readInt8(17);
+									console.log({
+										lsn,
+										timestamp,
+										shouldRespond
+									});
+									if (shouldRespond) {
+										standbyStatusUpdate(client, lastLsn);
+									}
+								} else {
+									console.log('Unknown message', msg.chunk[0]);
 								}
 
-								let lsn = (msg.chunk.readUInt32BE(1).toString(16).toUpperCase()) + '/' + (msg.chunk.readUInt32BE(5).toString(16).toUpperCase());
-								lastLsn = lsn;
-								pass.write({
-									lsn,
-									log: test_decoding.parse(msg.chunk.slice(25).toString('utf8'))
-								});
-							} else if (msg.chunk[0] == 0x6b) { // Primary keepalive message
-								let lsn = (msg.chunk.readUInt32BE(1).toString(16).toUpperCase()) + '/' + (msg.chunk.readUInt32BE(5).toString(16).toUpperCase());
-								var timestamp = Math.floor(msg.chunk.readUInt32BE(9) * 4294967.296 + msg.chunk.readUInt32BE(13) / 1000 + 946080000000);
-								var shouldRespond = msg.chunk.readInt8(17);
-								console.log({
-									lsn,
-									timestamp,
-									shouldRespond
-								});
-								if (shouldRespond) {
-									standbyStatusUpdate(client, lastLsn);
-								}
-							} else {
-								console.log('Unknown message', msg.chunk[0]);
-							}
-
+							});
 						});
 					});
 				});
 			});
 		});
+		retry.backoff();
 		return pass;
 	}
 };
