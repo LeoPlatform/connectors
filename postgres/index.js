eba616ee8d649c2943c6db65805a60ac261939ac
Francis Lewis <flewis@leoinsights.com>
2018-03-21 11:36:49 -0600

    Added domainObjectLoader to postgres. Everything else currently exists to make snapshotting work in postgres.

--- postgres/index.js
+++ postgres/index.js
@@ -3,6 +3,10 @@ const connect = require("./lib/connect.js");
 const sqlLoader = require("leo-connector-common/sql/loader");
 const sqlNibbler = require("leo-connector-common/sql/nibbler");
 const sqlSnapshotter = require("leo-connector-common/sql/snapshotter");
+const snapShotter = require("leo-connector-common/sql/snapshotter");
+
+const leo = require("leo-sdk");
+const ls = leo.streams;
 
 const binlogReader = require("./lib/binlogreader.js");
 
@@ -15,6 +19,20 @@ module.exports = {
 	},
 	snapshot: function(config, table, id, domain) {
 		return sqlSnapshotter(connect(config), table, id, domain);
+	},
+	domainObjectLoader: function(bot_id, dbConfig, sql, domain, opts, callback) {
+		if (opts.snapshot) {
+			snapShotter(bot_id, connect(dbConfig), dbConfig.table, dbConfig.id, domain, {
+				event: opts.outQueue
+			}, callback);
+		} else {
+			let stream = leo.read(bot_id, opts.inQueue);
+			let stats = ls.stats(bot_id, opts.inQueue);
+			ls.pipe(stream, this.load(dbConfig, sql, domain), leo.load(bot_id, opts.outQueue || dbConfig.table), err => {
+				if (err) return callback(err);
+				return stats.checkpoint(callback);
+			});
+		}
 	},
 	streamChanges: binlogReader.stream
 };
