b7fca9d678066e8abb1f548a7fcc51b42a6a5cbc
Francis Lewis <flewis@leoinsights.com>
2018-04-12 16:38:09 -0600

    Changed to checking for number of units instead of doing an object length check

--- sqlserver/index.js
+++ sqlserver/index.js
@@ -55,6 +55,7 @@ module.exports = {
 		let offset = parts[1] || 0;
 
 		let sqlTables = Object.keys(tables).map(t => {
+			obj.payload[t] = [];
 			let query = `SELECT '${t}' as tableName, ${tables[t]} as id, SYS_CHANGE_VERSION __SYS_CHANGE_VERSION
 					 FROM  CHANGETABLE(CHANGES ${t}, ${version - 1}) AS CT  
 					 where SYS_CHANGE_VERSION > ${version} OR (SYS_CHANGE_VERSION = ${version} AND ${tables[t]} > ${offset})`;
@@ -64,12 +65,7 @@ module.exports = {
 		client.query(sqlTables.join(" UNION ") + ' order by SYS_CHANGE_VERSION asc, id asc', (err, result) => {
 			logger.log(sqlTables.join(" UNION ") + ' order by SYS_CHANGE_VERSION asc, id asc');
 			if (!err) {
-
 				result.forEach(r => {
-					if (!obj.payload[r.tableName]) {
-						obj.payload[r.tableName] = [];
-					}
-
 					let eid = `${r.__SYS_CHANGE_VERSION}.${r.id}`;
 					obj.correlation_id.units++;
 					obj.correlation_id.start = obj.correlation_id.start || eid;
@@ -78,8 +74,7 @@ module.exports = {
 					obj.payload[r.tableName].push(r.id);
 				});
 
-				// only write to the stream if we have a payload
-				if (Object.keys(obj.payload).length > 0) {
+				if (obj.correlation_id.units > 0) {
 					stream.write(obj);
 				}
 			} else {
