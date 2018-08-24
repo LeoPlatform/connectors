e340c2d3bd093716a5a439beb2564c7355e36ee8
Clint Zirker <czirker@leoinsights.com>
2018-08-24 16:03:29 -0600

    Added checksum2 to mongo/index.js

--- mongo/index.js
+++ mongo/index.js
@@ -1,9 +1,13 @@
 "use strict";
 const connect = require("./lib/connect.js");
 const checksum = require("./lib/checksum.js");
+const checksum2 = require("./lib/checksum2.js");
 
 module.exports = {
-	checksum: function (config) {
+	checksum: function(config) {
 		return checksum(connect(config));
+	},
+	checksum2: function(config) {
+		return checksum2(connect(config));
 	}
 };
