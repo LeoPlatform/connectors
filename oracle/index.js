edabe92eaefa19d872701ae2d29649821c98bab0
Francis Lewis <flewis@leoinsights.com>
2018-08-28 19:46:02 -0600

    Oracle connector

---
+++ oracle/index.js
@@ -0,0 +1,33 @@
+"use strict";
+const connect = require("./lib/connect.js");
+const sqlLoader = require("leo-connector-common/sql/loader");
+const sqlLoaderJoin = require('leo-connector-common/sql/loaderJoinTable');
+const leo = require("leo-sdk");
+const ls = leo.streams;
+
+module.exports = {
+	load: function(config, sql, domain, opts, idColumns) {
+		if (Array.isArray(idColumns)) {
+			return sqlLoaderJoin(connect(config), idColumns, sql, domain, opts);
+		} else {
+			return sqlLoader(connect(config), sql, domain, opts);
+		}
+	},
+	domainObjectLoader: function(bot_id, dbConfig, sql, domain, opts, callback) {
+		if (opts.snapshot) {
+			throw new Error('Snapshotting not implemented yet in Oracle');
+			// snapShotter(bot_id, connect(dbConfig), dbConfig.table, dbConfig.id, domain, {
+			// 	event: opts.outQueue
+			// }, callback);
+		} else {
+			let stream = leo.read(bot_id, opts.inQueue, {start: opts.start});
+			let stats = ls.stats(bot_id, opts.inQueue);
+
+			ls.pipe(stream, stats, this.load(dbConfig, sql, domain, opts, dbConfig.id), leo.load(bot_id, opts.outQueue || dbConfig.table), err => {
+				if (err) return callback(err);
+				return stats.checkpoint(callback);
+			});
+		}
+	},
+	connect: connect
+};
