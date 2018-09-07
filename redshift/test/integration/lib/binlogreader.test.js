51deb99056245508a5a7f4d515aae68cd19ca043
Francis Lewis <flewis@leoinsights.com>
2018-09-07 17:02:09 -0600

    Delete binlogreader.test.js
    removing the binglogreader test

--- redshift/test/integration/lib/binlogreader.test.js
+++
@@ -1,9 +0,0 @@
-require("chai").should();
-// const loader = require("../");
-
-describe.skip('SQL', function() {
-	it('Should be binlog read from postgres', function(done) {
-		this.timeout(1000 * 500);
-		done("INCOMPLETE");
-	});
-});
