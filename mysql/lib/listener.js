d139e706b9e1708b842d5f52125b1ae365d9d9f9
Clint Zirker <czirker@leoinsights.com>
2018-09-13 16:20:02 -0600

    Rework for binlog change stream

--- mysql/lib/listener.js
+++ mysql/lib/listener.js
@@ -2,6 +2,11 @@
 
 const ZongJi = require("zongji");
 const ls = require('leo-streams');
+const moment = require("moment");
+const logger = require("leo-logger")("mysql").sub("binloglistener");
+logger.configure(/mysql\.binloglistener/, {
+	info: true
+});
 
 let typeMap = {
 	writerows: 'update',
@@ -14,6 +19,10 @@ ZongJi.prototype._fetchTableInfo = function(tableMapEvent, next) {
 	var sql = `SELECT COLUMN_NAME, COLLATION_NAME, CHARACTER_SET_NAME, COLUMN_COMMENT, COLUMN_TYPE, COLUMN_KEY
 		FROM information_schema.columns WHERE table_schema='${tableMapEvent.schemaName}' 
 		AND table_name='${tableMapEvent.tableName}'`;
+
+	if (self.__ended) {
+		return;
+	}
 
 	this.ctrlConnection.query(sql, function(err, rows) {
 		if (err) {
@@ -45,11 +54,35 @@ ZongJi.prototype._fetchTableInfo = function(tableMapEvent, next) {
 ZongJi.prototype.start = function(options) {
 	var self = this;
 	self.set(options);
+
 	let pass = ls.passthrough({
 		objectMode: true
 	});
+
+	let passEnd = pass.end;
+	pass.end = function() {
+		self.__ended = true;
+		self.connection.destroy();
+
+		logger.debug("Ending binglog stream");
+		self.ctrlConnection.query(
+			'KILL ' + self.connection.threadId,
+			function() {
+				if (self.ctrlConnectionOwner) {
+					self.ctrlConnection.destroy();
+				}
+				passEnd.call(pass);
+			}
+		);
+	};
+
 	var _start = function() {
 		self.connection._implyConnect();
+		if (self.options.binlogName) {
+			logger.log(`Starting from ${self.options.source} ${self.options.binlogName}::${self.options.binlogNextPos}`);
+		} else {
+			logger.log(`Starting from ${self.options.source} latest`);
+		}
 		self.connection._protocol._enqueue(new self.binlog(function(error, event) {
 			if (error) return self.emit('error', error);
 			// Do not emit events that have been filtered out
@@ -71,7 +104,6 @@ ZongJi.prototype.start = function(options) {
 					return;
 					break;
 				case 'rotate':
-					console.log(self.binlogNextPos);
 					if (self.binlogName !== event.binlogName) {
 						self.binlogName = event.binlogName;
 					}
@@ -80,15 +112,13 @@ ZongJi.prototype.start = function(options) {
 			}
 			self.binlogNextPos = event.nextPosition;
 
-
-
 			let tableName = event.tableMap[event.tableId].tableName;
 			let schema = event.tableMap[event.tableId].parentSchema;
-			if (!pass.write({
+			if (event.tableMap[event.tableId].primaryKey.length && !pass.write({
 					timestamp: event.timestamp,
 					event_source_timestamp: event.timestamp,
 					correlation_id: {
-						source: 'system:mysql',
+						source: self.options.source || 'system:mysql',
 						start: self.binlogName + "::" + event.nextPosition
 					},
 					payload: {
@@ -125,19 +155,47 @@ module.exports = function(config, opts) {
 			performance_schema: true,
 			sys: true
 		},
-		fullEvent: false
+		fullEvent: false,
+		startAtEnd: true
 	}, opts || {});
 
 
-	let z = new ZongJi(config);
-	let stream = z.start({
-		binlogName: "master.000024",
-		binlogNextPos: 0,
+	let position = opts.position || {};
+	if (typeof position === "string") {
+		let [binlogName, binlogNextPos] = position.split("::");
+		position = {
+			binlogName,
+			binlogNextPos
+		}
+	}
+
+	let startParams = {
+		binlogName: position.binlogName,
+		binlogNextPos: parseInt(position.binlogNextPos || 0),
+		startAtEnd: opts.startAtEnd,
 		serverId: opts.server_id,
 		includeEvents: ['rotate', 'tablemap', 'writerows', 'updaterows', 'deleterows'],
 		includeSchema: opts.includeSchema,
-		excludeSchema: opts.excludeSchema
-	});
+		excludeSchema: opts.excludeSchema,
+		source: opts.source
+	}
+	// Require a Starting location
+	if (!startParams.binlogName) {
+		throw new Error("opts.binlogName and opts.binlogNextPos are required");
+	}
+
+	let z = new ZongJi(config);
+	let stream = z.start(startParams);
+
+	let timeoutHandle;
+	if (opts.stopTime) {
+		timeoutHandle = setTimeout(() => stream.end(), Math.min(2147483647, moment(opts.stopTime) - moment()));
+	} else if (opts.duration) {
+		timeoutHandle = setTimeout(() => stream.end(), Math.min(2147483647, moment.duration(opts.duration).asMilliseconds()));
+	}
+	if (timeoutHandle) {
+		stream.on("finish", () => clearTimeout(timeoutHandle));
+	}
 
 	if (opts.fullEvent) {
 		return stream;
